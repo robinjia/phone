0462d4e539b4581ce6e62b567b510ad26a50e951
Robin Jia <robinjia@stanford.edu>
2015-01-24 21:36:03 -0800

    Tries reducing framerate

--- phone.py
+++ phone.py
@@ -8,7 +8,7 @@ READY_MESSAGE = 'ready'
 # Audio Constants
 SAMPLE_WIDTH = 2  # bytes per channel
 NUM_CHANNELS = 2  # number of channels
-FRAME_RATE = 44100  # frame rate in Hz
+FRAME_RATE = 10000  # frame rate in Hz
 INPUT_FORMAT = pyaudio.paInt16  # Format for microphone (2-bytes)
 INPUT_FRAMES_PER_BUFFER = 32
 OUTPUT_FRAMES_PER_BUFFER = 32
