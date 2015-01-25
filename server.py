a05eff2e2c059d6b445cea06095273895fa76aff
Robin Jia <robinjia@stanford.edu>
2015-01-25 13:53:15 -0800

    Some attempted optimizations
    Saving work before going in a different direction

--- server.py
+++ server.py
@@ -25,20 +25,43 @@ def main():
 
     Pulls in more data from the connection.
     """
-    data = ''
     bytes_needed = phone.SAMPLE_WIDTH * phone.NUM_CHANNELS * frame_count
-    while bytes_needed > 0:
-      new_data = connection.recv(bytes_needed)
-      if len(new_data) < bytes_needed:
-        print >> sys.stderr, 'Received %d bytes (%d needed)' % (
-            len(new_data), bytes_needed)
-      data += new_data
-      bytes_needed -= len(new_data)
-    # connection.sendall(phone.ACK_MESSAGE)
-    return (data, pyaudio.paContinue)
+
+    # Read a big chunk now
+    new_data = connection.recv(2 * bytes_needed)
+    print >> sys.stderr, 'Initial read: data = %d, new = %d bytes' % (
+       len(data[0]), len(new_data))
+    data[0] += new_data
+    
+    # If that wasn't enough, wait until we've read bytes_needed
+    while len(data[0]) < bytes_needed:
+      print >> sys.stderr, 'Still need more data...'
+      new_data = connection.recv(bytes_needed - len(data[0]))
+      data[0] += new_data
+
+    """
+    if len(data[0]) - bytes_needed >= phone.MAX_BYTES_BEHIND:
+      # We've fallen too far behind--catch up now.
+      print >>sys.stderr, 'catching up'
+      ret = data[0][len(data[0]) - phone.DESIRED_BYTES_BEHIND - bytes_needed:
+                 len(data[0]) - phone.DESIRED_BYTES_BEHIND]
+      data[0] = data[0][len(data[0]) - phone.DESIRED_BYTES_BEHIND:]
+    else:
+    """
+    # We're okay, just return from the beginning of data
+    # print >>sys.stderr, 'coasting'
+    start = time.clock()
+    ret = data[0][:bytes_needed]
+    data[0] = data[0][bytes_needed:]
+    print 'accumulated in buffer %d bytes' % len(data[0])
+    end = time.clock()
+    print >> sys.stderr, 'get_data took %g seconds' % (end - start)
+
+    return (ret, pyaudio.paContinue)
 
   # Initialize socket
   sock = InitSocket()
+  data = ['']  # Silly hack to let the inner function modify data
 
   # Make a connection
   connection, client_address = sock.accept()
@@ -59,12 +82,12 @@ def main():
     # Wait for stream to finish
     while stream.is_active():
       time.sleep(0.1)
+    print >> sys.stderr, 'Stream stopped'
   finally:
     connection.close()
 
   stream.stop_stream()
   stream.close()
-  wf.close()
   p.terminate()
 
 
